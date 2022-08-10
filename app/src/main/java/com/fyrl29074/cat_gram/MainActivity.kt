f39796fb05a942ef1f6668b9369cfaa377cee89e
Kim Oleg <Fyrl29074@gmail.com>
2022-08-10 08:33:39 +0300

    init

---
+++ app/src/main/java/com/fyrl29074/cat_gram/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.fyrl29074.cat_gram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fyrl29074.cat_gram.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
